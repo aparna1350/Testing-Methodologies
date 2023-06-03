30eb3bdba293737ae5c54cfd011ef4914476b054
aparna1350 <113434470+aparna1350@users.noreply.github.com>
2023-06-03 12:44:59 +0530

    Add files via upload

---
+++ test5/src/main/java/com/example/demo/testing/App.java
@@ -0,0 +1,15 @@
+package com.example.demo.testing;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
